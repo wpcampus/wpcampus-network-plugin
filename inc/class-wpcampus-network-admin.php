68b54cd9bac2c43032d5efcf7110e8af03ca5921
Rachel Cherry <bamadesigner@gmail.com>
2020-02-15 10:55:42 -0500

    Add WPCampus settings page

--- inc/class-wpcampus-network-admin.php
+++ inc/class-wpcampus-network-admin.php
@@ -9,6 +9,11 @@
  */
 class WPCampus_Network_Admin {
 
+	const OPTIONS_TITLE = 'WPCampus: Settings';
+	const OPTIONS_MENU_TITLE = 'WPCampus';
+	const OPTIONS_MENU_SLUG = 'wpcampus-settings';
+	const OPTIONS_USER_CAP = 'manage_options';
+
 	/**
 	 * We don't need to instantiate this class.
 	 */
@@ -21,6 +26,8 @@ class WPCampus_Network_Admin {
 		$plugin = new self();
 
 		add_action( 'network_admin_menu', array( $plugin, 'add_network_admin_pages' ) );
+
+		add_action( 'admin_menu', [ $plugin, 'add_admin_pages' ] );
 
 		add_action( 'admin_menu', array( $plugin, 'modify_admin_menu' ) );
 		add_action( 'edit_form_after_title', array( $plugin, 'print_meta_boxes_after_title' ), 0 );
@@ -46,6 +53,61 @@ class WPCampus_Network_Admin {
 	}
 
 	/**
+	 * Registers an ACF options page.
+	 */
+	public function add_admin_pages() {
+
+		if ( ! function_exists( 'acf_add_options_page' ) ) {
+			$this->add_default_options_page();
+			return;
+		}
+
+		acf_add_options_page(
+			[
+				'page_title' => self::OPTIONS_TITLE,
+				'menu_title' => self::OPTIONS_MENU_TITLE,
+				'capability' => self::OPTIONS_USER_CAP,
+				'menu_slug'  => self::OPTIONS_MENU_SLUG,
+			] );
+
+		$this->add_acf_fields();
+	}
+
+	/**
+	 * We only add this page if ACF isn't enabled.
+	 */
+	private function add_default_options_page() {
+
+		add_menu_page(
+			self::OPTIONS_TITLE,
+			self::OPTIONS_MENU_TITLE,
+			self::OPTIONS_USER_CAP,
+			self::OPTIONS_MENU_SLUG,
+			[ $this, 'print_default_options_page' ]
+		);
+	}
+
+	/**
+	 * Print default options page which tells the user
+	 * to enable ACF in order to access settings.
+	 */
+	public function print_default_options_page() {
+		?>
+		<div class="wrap">
+			<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+			<p>WPCampus settings require the Advanced Custom Fields PRO plugin. <a href="<?php echo admin_url( 'plugins.php' ); ?>">Manage plugins</a></p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Adds/registers our ACF fields.
+	 */
+	private function add_acf_fields() {
+		require_once plugin_dir_path( __FILE__ ) . 'wpcampus-network-fields.php';
+	}
+
+	/**
 	 * Print our main management page.
 	 */
 	public function print_manage_network_page() {
@@ -65,7 +127,7 @@ class WPCampus_Network_Admin {
 					<div class="notice notice-error is-dismissible">
 						<p><?php _e( "The action couldn't be completed because you do not have authentication.", 'wpcampus-network' ); ?></p>
 					</div>
-					<?php
+				<?php
 				else :
 
 					// Add all users to all blogs.
@@ -75,7 +137,7 @@ class WPCampus_Network_Admin {
 					<div class="notice notice-success is-dismissible">
 						<p><?php _e( 'All users were added to all blogs.', 'wpcampus-network' ); ?></p>
 					</div>
-					<?php
+				<?php
 				endif;
 			endif;
 
@@ -84,13 +146,13 @@ class WPCampus_Network_Admin {
 
 			//if ( current_user_can( 'manage_wpc_network_users' ) ) {
 			$actions[] = array(
-				'href' => wp_nonce_url(
+				'href'  => wp_nonce_url(
 					add_query_arg(
 						array(
 							'page' => $page_slug,
-					        //'wpc_action' => 'add_all_users_to_all_blogs',
+							//'wpc_action' => 'add_all_users_to_all_blogs',
 						),
-				        network_admin_url( 'admin.php' )
+						network_admin_url( 'admin.php' )
 					),
 					'add_all_users_to_all_blogs',
 					'wpc_nonce'
@@ -102,7 +164,7 @@ class WPCampus_Network_Admin {
 			if ( empty( $actions ) ) :
 				?>
 				<p><?php _e( "There's nothing for you to do.", 'wpcampus-network' ); ?></p>
-				<?php
+			<?php
 			else :
 				?>
 				<ul style="list-style:disc;margin-left:2em;">
@@ -111,12 +173,12 @@ class WPCampus_Network_Admin {
 					foreach ( $actions as $action ) :
 						?>
 						<li><a href="<?php echo $action['href']; ?>"><?php echo $action['label']; ?></a></li>
-						<?php
+					<?php
 					endforeach;
 
 					?>
 				</ul>
-				<?php
+			<?php
 			endif;
 
 			?>
